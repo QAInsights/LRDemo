58ec0a12b522698cc7f10801c27121f1a0c8fe5b
NaveenKumar <QAInsights@users.noreply.github.com>
2018-04-18 22:44:47 -0500

    This is my first commit

---
+++ Action.c
@@ -0,0 +1,16 @@
+Action()
+{
+
+
+	web_custom_request("web_custom_request",
+		"URL=www.example.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	
+	return 0;
+}
